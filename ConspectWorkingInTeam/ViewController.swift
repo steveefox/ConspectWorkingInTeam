fdd6f0c865fad4f7a405e8d91adf05c0431987b4
Nikita Begletskiy <steveefox50@icloud.com>
2020-06-13 23:55:26 +0300

    Initial Commit

---
+++ ConspectWorkingInTeam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ConspectWorkingInTeam
+//
+//  Created by Nikita on 6/13/20.
+//  Copyright © 2020 Nikita Begletskiy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
